34709f43e88bb13575e031c572d72a5aa32f79e0
Anish Gaur <54688001+anish8808@users.noreply.github.com>
2026-04-11 16:48:50 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.go
@@ -0,0 +1,19 @@
+func majorityElement(nums []int) int {
+    candidate := nums[0]
+    count :=1
+    for i:=1; i<len(nums); i++{
+        
+        if (nums[i] == candidate)  {
+            count++
+        }else {
+            count--
+        }
+
+        if(count<=0) {
+            candidate = nums[i]
+        }
+    }
+
+    return candidate
+
+}
