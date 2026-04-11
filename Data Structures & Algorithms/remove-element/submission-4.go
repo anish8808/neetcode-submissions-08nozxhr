a61c12748e2fbd37085d706735b825e98eb31cb5
Anish Gaur <54688001+anish8808@users.noreply.github.com>
2026-04-11 16:33:27 +0530

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.go
@@ -0,0 +1,14 @@
+func removeElement(nums []int, val int) int {
+    l , r := 0 , len(nums)-1
+
+    for l<=r{
+        if (nums[l]==val) {
+            nums[l] , nums[r] =  nums[r] , nums[l]
+            r--
+        }else {
+            l++
+        }
+    }
+
+    return r+1
+}
